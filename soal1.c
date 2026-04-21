61aedaa9f255276386c1a7f16d90206595650207
stockfishZ <rafasatria2512@gmail.com>
2026-04-21 09:35:42 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -5,30 +5,49 @@
 #include <stdio.h>
 #include <string.h>
 
-void check(char *st){
+void checkbalance(char *st, int* open, int* close){
+    int open_temp = 0; int close_temp = 0;
     for (int i = 0; i < strlen(st); i++){
-        int found = 0;
         if (st[i] == '('){
-            for (int j = i+1; j < strlen(st); j++){
-                if (found != 1){
+            open_temp++;
+        }else if (st[i] == ')'){
+            close_temp++;
+        }
+    }
+
+    *open = open_temp;
+    *close = close_temp;
+}
+
+void check(char *st){
+    int open = 0; int close = 0;
+    int lastidx = 1000;
+    for (int i = 0; i < strlen(st); i++){
+        if (st[i] == '('){
+            if (lastidx == 1000){
+                for (int j = strlen(st); j > i; j--){
                     if (st[j] == '('){
                         memmove(&st[j], &st[j + 1], strlen(st) - j);
                     }else if (st[j] == ')'){
-                        found = 1;
-                    }
-                }else{
-                    if (st[j] == '('){
+                        lastidx = j;
                         break;
-                    }else if (st[j] == ')'){
+                    }
+                }
+            }else{
+                for (int j = lastidx; j > i; j--){
+                    if (st[j] == '('){
                         memmove(&st[j], &st[j + 1], strlen(st) - j);
+                    }else if (st[j] == ')'){
+                        lastidx = j;
+                        break;
                     }
                 }
             }
-            if (found ==  0){
+        }else if (st[i] == ')'){
+            checkbalance(st, &open, &close);
+            if (open != close){
                 memmove(&st[i], &st[i + 1], strlen(st) - i);
             }
-        }else if (st[i] == ')'){
-            memmove(&st[i], &st[i + 1], strlen(st) - i);
         }
     }
 }
