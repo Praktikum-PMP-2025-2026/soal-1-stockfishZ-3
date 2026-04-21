6edc2556e726c57bb671260521a2f41d7593ec10
stockfishZ <rafasatria2512@gmail.com>
2026-04-21 09:59:08 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -5,57 +5,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void checkbalance(char *st, int* open, int* close){
-    int open_temp = 0; int close_temp = 0;
-    for (int i = 0; i < strlen(st); i++){
-        if (st[i] == '('){
-            open_temp++;
-        }else if (st[i] == ')'){
-            close_temp++;
-        }
-    }
-
-    *open = open_temp;
-    *close = close_temp;
-}
-
 void check(char *st){
-    int open = 0; int close = 0;
-    int lastidx = 1000;
-    for (int i = 0; i < strlen(st); i++){
+    for (int i = 0; i < strlen(st)/2; i++){
+        int found = 0;
         if (st[i] == '('){
-            if (lastidx == 1000){
-                for (int j = strlen(st); j > i; j--){
+            for (int j = i+1; j < strlen(st); j++){
+                if (found != 1){
                     if (st[j] == '('){
                         memmove(&st[j], &st[j + 1], strlen(st) - j);
                     }else if (st[j] == ')'){
-                        lastidx = j;
-                        break;
+                        found = 1;
                     }
-                }
-            }else{
-                for (int j = lastidx; j > i; j--){
+                }else{
                     if (st[j] == '('){
-                        memmove(&st[j], &st[j + 1], strlen(st) - j);
-                    }else if (st[j] == ')'){
-                        lastidx = j;
                         break;
+                    }else if (st[j] == ')'){
+                        memmove(&st[j], &st[j + 1], strlen(st) - j);
                     }
                 }
             }
-        }else if (st[i] == ')'){
-            checkbalance(st, &open, &close);
-            if (open != close){
+            if (found ==  0){
                 memmove(&st[i], &st[i + 1], strlen(st) - i);
             }
+        }else if (st[i] == ')'){
+            memmove(&st[i], &st[i + 1], strlen(st) - i);
         }
     }
 }
 
 int main(){
     char st[100];
-
-    scanf("%s", st);
+    fgets(st, sizeof(st), stdin);
     check(st);
     printf("%s", st);
     return 0;
